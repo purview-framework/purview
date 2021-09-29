6b3b60b50500d865e78ff77fa1f5b879e6416e36
Ian <bontaq@gmail.com>
2021-09-29 06:44:46 -0400

    Finally renders subcomponents

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import           Prelude      hiding (div)
 import           Data.Text    hiding (count)
+import           Data.Aeson
+import           GHC.Generics
 import           Lib
 
 newtype State = State
@@ -13,12 +16,23 @@ defaultCounterState = State { count = 0 }
 data Action
   = Increment
   | Decrement
-  deriving (Show, Read)
+  deriving (Show, Read, Generic)
+
+instance FromJSON Action where
 
 handlers' :: State -> Action -> State
 handlers' state message = case message of
   Increment -> state { count = count state + 1 }
   Decrement -> state { count = count state - 1 }
+
+other :: Component (Maybe String) String
+other = Component
+  { state = Nothing
+  , handlers = \state message -> state
+  , render = \state -> div [] [ text "and me" ]
+  }
+
+feh = SomeComponent other
 
 render' :: State -> Html Action
 render' state =
@@ -26,9 +40,10 @@ render' state =
     [ div [ onClick Increment ] [ text "increment" ]
     , text ("count: " <> show (count state))
     , div [ onClick Decrement ] [ text "decrement" ]
+    , feh
     ]
 
-counter = Component
+counter = MkSomeComponent $ Component
   { state    = defaultCounterState
   , handlers = handlers'
   , render   = render'
