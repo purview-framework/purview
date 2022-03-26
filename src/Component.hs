faad3f6beb5d69163e895a6fcd7fe9db2c2ceace
Ian <bontaq@gmail.com>
2022-03-26 17:40:35 -0400

    Remove the "Jack" idea

--- src/Component.hs
+++ src/Component.hs
@@ -46,10 +46,6 @@ data Purview a where
     -> (state -> Purview action)
     -> Purview action
 
-  Jack
-    :: ((action -> IO ()) -> Purview a) -- maybe?
-    -> Purview a
-
   Once
     :: (ToJSON action)
     => ((action -> FromEvent) -> FromEvent)
@@ -63,7 +59,6 @@ instance Show (Purview a) where
   show (EffectHandler location state _action cont) = "EffectHandler " <> show location <> " " <> show (cont state)
   show (MessageHandler location state _action cont) = "MessageHandler " <> show location <> " " <> show (cont state)
   show (Once _ hasRun cont) = "Once " <> show hasRun <> " " <> show cont
-  show (Jack cont) = "Jack " <> show (cont (const undefined))
   show (Attribute _attrs cont) = "Attr " <> show cont
   show (Text str) = show str
   show (Html kind children) =
@@ -354,11 +349,6 @@ prepareGraph' location component = case component of
           rest = prepareGraph' location cont
         in
           (Once effect True (fst rest), snd rest)
-
-  Jack cont ->
-    let send message = undefined
-        rest = prepareGraph' location (cont send)
-    in (Jack cont, snd rest)
 
   Hide x ->
     let (child, actions) = prepareGraph' location x
