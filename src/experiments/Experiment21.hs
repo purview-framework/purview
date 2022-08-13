7e5e75003b005cc151405089089fbbc790446a25
Ian <bontaq@gmail.com>
2022-08-13 12:43:37 -0400

    Alright this is good to implement
    I'm convinced

--- src/experiments/Experiment21.hs
+++ src/experiments/Experiment21.hs
@@ -29,13 +29,13 @@ Senders (triggered by events in the HTML) can only send events to the
 handler above them in the graph.
 
 There will be few handlers / senders, and a lot of Html. Having the
-parentEvent on Senders and Html doesn't fit the model, since it's
+event on Senders and Html doesn't fit the model, since it's
 not needed.  Is there a better way to model this that would remove
-parentEvent, but still keep track of it?  New Handlers can be anywhere
+event, but still keep track of it?  New Handlers can be anywhere
 in the tree and need to know what events it can send to the parent.
 
 If there was some way for the Handler to "know" that the "action" type
-of the Sender / Html it's embedded in was its "parentEvent" type, that'd
+of the Sender / Html it's embedded in was its "event" type, that'd
 be pretty cool but might not be possible?
 
 I've been wondering if this could be solved with implicit params or
@@ -45,60 +45,57 @@ something monadic, but I'm not sure.
 
 data Event a b = Parent a | Self b
 
-data Graph parentEvent event where
+type Reducer event newEvent = Graph event
+
+data Graph event where
   Sender
     :: event
-    -> [Graph parentEvent event]
+    -> [Graph event]
     -- ^ children
-    -> Graph parentEvent event
+    -> Graph event
   Handler ::
-    { handler :: newEvent -> [Event parentEvent newEvent]
+    { handler :: newEvent -> [Event event newEvent]
     -- ^ receives the sender event and generates new events
-    , children :: [Graph newEvent any]
+    , children :: [Graph newEvent]
     }
-    -> Graph parentEvent newEvent
+    -> Reducer event newEvent
   Html
     :: String
-    -> [Graph parentEvent event]
+    -> [Graph event]
     -- ^ children
-    -> Graph parentEvent event
+    -> Graph event
 
 data Vertical = North | South
 data Horizontal = East | West
 
-data RegistrationFormEvent = UserInfoSubmitted { email :: Maybe String, username :: Maybe String }
+-- here you can see where having the event type makes less sense
+-- westSender :: Graph event
+westSender = Sender East [ Html "button" [] ]
 
-registrationForm = Sender UserInfoSubmitted
-  [ Html "form"
-      [ -- email input
-        -- username input
-        -- submit button
-      ]
-  ]
-
-registrationHandler = Handler handle
-  where handle UserInfoSubmitted { name, username } = do
-          -- check the name is valid
-          -- check the username is valid
-          --
-
--- here you can see where having the parentEvent type makes less sense
-westSender :: Graph parentEvent Horizontal
-westSender = Sender West [ Html "button" [] ]
-
-child :: Graph Vertical Horizontal
+-- child :: Graph Vertical
 child = Handler (\event -> [ Parent North, Self East ]) [ westSender ]
 
-parent :: [Graph Vertical any] -> Graph () Vertical
 parent = Handler (\event -> [ Self North ])
 
-graph :: Graph () Vertical
 graph = parent [ child ]
 
--- I see I can do something like this:
-type Sender event = forall parentEvent. Graph parentEvent event
+data Temp where
+  Temp :: { value :: a, value2 :: a } -> Temp
 
--- to hide having to type out the parentEvent, but it's still not
--- exactly matching with how it really works
-eastSender :: Sender Horizontal
-eastSender = Sender East []
+test = Temp () ""
+
+{- Checks
+
+- handler / handler mismatch
+- sender / handler mismatch
+
+-}
+
+
+-- -- I see I can do something like this:
+-- type Sender event = forall event. Graph event event
+--
+-- -- to hide having to type out the event, but it's still not
+-- -- exactly matching with how it really works
+-- eastSender :: Sender Horizontal
+-- eastSender = Sender East []
