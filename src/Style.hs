5776ec08f4e01802d66ddbac0b80536f1576e4d4
Ian <bontaq@gmail.com>
2023-06-03 21:02:17 -0400

    Quasiquotes rock actually?

---
+++ src/Style.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE TemplateHaskell, DeriveLift, QuasiQuotes #-}
+
+-- |
+module Style
+  ( style
+  , Style (..)
+  )
+where
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
+
+import Data.Bits
+import Data.List
+
+-- thanks https://stackoverflow.com/questions/59399050/haskell-making-quasi-quoted-values-strict-evaluated-at-compile-time
+
+newtype Style = Style (String, String)
+  deriving (Show, Eq)
+
+style' :: String -> Q Exp
+style' css =
+  let hashed = show $ hash css
+  in [| Style (hashed, css) |]
+
+style :: QuasiQuoter
+style = QuasiQuoter
+  { quoteDec = error "quoteDec not implemented"
+  , quoteType = error "quoteType not implemented"
+  , quotePat = error "quotePat not implemented"
+  , quoteExp = style'
+  }
+
+-- snagged from https://stackoverflow.com/a/9263004/1361890
+hash :: String -> Int
+hash = foldl' (\h c -> 33 * h `xor` fromEnum c) 5381
---
+++ src/StyleSpec.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+-- |
+
+module StyleSpec where
+
+import Test.Hspec
+
+import Style (Style (..), style)
+
+spec :: SpecWith ()
+spec = parallel $ do
+  describe "the style quasiquoter" $ do
+    it "produces a non-empty style" $ do
+      let Style (hash, style') = [style|blue;|]
+
+      hash `shouldSatisfy` (not . null)
+      style' `shouldBe` "blue;"
+
+    it "produces the same hash for the same style" $ do
+      let
+        styleA = [style|blue;|]
+        styleB = [style|blue;|]
+
+      styleA `shouldBe` styleB
+
+    it "produces different hashes for different styles" $ do
+      let
+        Style (hashA, _) = [style|blue;|]
+        Style (hashB, _) = [style|red;|]
+
+      hashA `shouldNotBe` hashB
